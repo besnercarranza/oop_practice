4343decd2c5c8899bbd5f35b9bac6c2581b1f344
besnercarranza <besnercarranza@mail.weber.edu>
2024-11-19 18:19:20 -0700

    define methods with a class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from vehicle import vehicle
 
 if __name__ == "__main__":
+   
     #create an instace of the Vehicle class
     black_car = vehicle("Tesla", "Model 3", 2018)
+
+    print(black_car)
