9d49fef95096ba4501e9e5f64806322e033a2d11
besnercarranza <besnercarranza@mail.weber.edu>
2024-11-19 18:34:40 -0700

    added class method

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ if __name__ == "__main__":
    
     #create an instace of the Vehicle class
     black_car = vehicle("Tesla", "Model 3", 2018)
+    red_car = vehicle("Toyota", "Camry", 2023 )
 
-    print(black_car)
+all_vehicles = vehicle.get_all_vehicles()
+for vehicles in all_vehicles:
+    print(vehicle)
--- vehicle.py
+++ vehicle.py
@@ -2,13 +2,21 @@
 class vehicle: 
 
     wheels = 4 # class level attribute assigned to all objects from the class
-   
+    all_vehicles = []
+
     def __init__(self, make, model, year):
         self.make = make
         self.model = model 
         self.year = year
 
-        # method that we can perform on the object
+        vehicle.all_vehicles.append(self)
+
+        # instance method that we can perform on the object
         def __str__(self):
             #return a string representing the vehicle object
             return f"vehicle: {self.make}, Model {self.model}, Year: {self.year}"
+
+        @classmethod
+        def get_all_vehicles(cls):
+            return cls.all_vehicles
+
