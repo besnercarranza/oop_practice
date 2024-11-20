65291b8ddb9fd1a1173c79d0af0753e65faab66f
besnercarranza <besnercarranza@mail.weber.edu>
2024-11-19 17:57:04 -0700

    creation of vehicle class with attributes

---
+++ vehicle.py
@@ -0,0 +1,9 @@
+# Parent Class
+class vehicle: 
+
+    wheels = 4 # class level attribute assigned to all objects from the class
+   
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model 
+        self.year = year
